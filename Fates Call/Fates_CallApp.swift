c0f09f0bac0c199ae745b1fa18319f2761e0a5f8
Mayun Suryatama <mayunngrh@gmail.com>
2026-05-07 00:38:50 +0800

    Initial Commit

---
+++ Fates Call/Fates_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fates_CallApp.swift
+//  Fates Call
+//
+//  Created by Mayun Suryatama on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Fates_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
